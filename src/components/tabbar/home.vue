74346ed5690693c190afe5c2bf03db42b80a7471
hxz1993 <1432438153@qq.com>
2018-05-03 21:28:40 +0800

    首页功能完成

--- src/components/tabbar/home.vue
+++ src/components/tabbar/home.vue
@@ -1,13 +1,84 @@
 <template>
-  <div>这是首页</div>
+  <div class="page">
+    <mt-swipe :auto="4000">
+    <mt-swipe-item v-for="(v,i) in pics" :key="i">
+      <img :src="v.img" alt="">
+    </mt-swipe-item> 
+  </mt-swipe>
+
+  <ul class="mui-table-view mui-grid-view">
+    <li class="mui-table-view-cell mui-media mui-col-xs-6">
+        <router-link to="/news">
+            <img class="mui-media-object" src="../../assets/images/menu1.png">
+            <div class="mui-media-body">新闻资讯</div>
+        </router-link>
+    </li>
+    <li class="mui-table-view-cell mui-media mui-col-xs-6">
+        <router-link to="/pics">
+          <img class="mui-media-object" src="../../assets/images/menu2.png">
+          <div class="mui-media-body">图片分享</div>
+        </router-link>
+    </li>
+    <li class="mui-table-view-cell mui-media mui-col-xs-6">
+        <router-link to="/goods"><img class="mui-media-object" src="../../assets/images/menu3.png">
+          <div class="mui-media-body">商品购买</div>
+        </router-link>
+    </li>
+    <li class="mui-table-view-cell mui-media mui-col-xs-6">
+        <router-link to="/">
+          <img class="mui-media-object" src="../../assets/images/menu4.png">
+          <div class="mui-media-body">留言反馈</div>
+        </router-link>
+    </li>
+    <li class="mui-table-view-cell mui-media mui-col-xs-6">
+        <router-link to="/">
+          <img class="mui-media-object" src="../../assets/images/menu5.png">
+          <div class="mui-media-body">视频专区</div>
+        </router-link>
+    </li>
+    <li class="mui-table-view-cell mui-media mui-col-xs-6">
+        <router-link to="/">
+          <img class="mui-media-object" src="../../assets/images/menu6.png">
+          <div class="mui-media-body">联系我们</div>
+        </router-link>
+    </li>
+	</ul>    
+  </div>
 </template>
 <script>
+import axios from "axios"
 export default {
-  
+  data(){
+    return {
+      pics:[]
+    }
+  },
+  created(){
+    axios({
+      url:"http://www.escook.cn:3000/api/getlunbo"
+    }).then(res=>{
+      if(res.data.status==0){
+        this.pics=res.data.message
+        console.log(res.data.message);
+
+      }
+    })
+  }
 }
 </script>
 <style>
-
+  .mint-swipe {
+    height: 250px;
+  }
+  .mint-swipe img {
+    width: 100%;
+  }
+  .mui-table-view.mui-grid-view .mui-table-view-cell {
+    width: 33.33%;
+  }
+  .mui-table-view.mui-grid-view .mui-table-view-cell .mui-media-object {
+    width: 60%;
+  }
 </style>
 
 
--- src/main.js
+++ src/main.js
@@ -6,6 +6,16 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+import MintUI from 'mint-ui';
+import 'mint-ui/lib/style.css';
+Vue.use(MintUI);
+
+
+//引入mui的样式
+import "./assets/mui/css/mui.css"
+import "./assets/mui/css/icons-extra.css"
+
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
